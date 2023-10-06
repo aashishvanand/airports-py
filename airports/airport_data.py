73c41e1a1ac88f1d4f8169d51420ecda593ad16a
Aashish Vivekanand <aashishvanand@gmail.com>
2023-10-06 11:10:57 +0800

    Added Error Handling

--- airports/airport_data.py
+++ airports/airport_data.py
@@ -2,27 +2,34 @@ import gzip
 import json
 import os
 
-# Load the gzipped data
-data_path = os.path.join(os.path.dirname(__file__), 'data', 'airports.gz')
-with gzip.open(data_path, 'rt') as f:
-    AIRPORTS_DATA = json.load(f)
+# Load the gzipped JSON data
+with gzip.open(os.path.join(os.path.dirname(__file__), 'airports.gz'), 'rt') as f:
+    airports = json.load(f)
 
 def get_airport_by_iata(iata_code):
-    """Retrieve airport data by IATA code."""
-    return [airport for airport in AIRPORTS_DATA if airport['iata'] == iata_code]
+    if len(iata_code) != 3:
+        raise ValueError("Invalid IATA format. Please provide a 3-letter uppercase code, e.g., 'AAA'.")
+    return [airport for airport in airports if airport["iata"] == iata_code.upper()]
 
 def get_airport_by_icao(icao_code):
-    """Retrieve airport data by ICAO code."""
-    return [airport for airport in AIRPORTS_DATA if airport['icao'] == icao_code]
+    if len(icao_code) != 4:
+        raise ValueError("Invalid ICAO format. Please provide a 4-letter uppercase code, e.g., 'VOMM'.")
+    return [airport for airport in airports if airport["icao"] == icao_code.upper()]
 
 def get_airport_by_city_code(city_code):
-    """Retrieve airport data by city code."""
-    return [airport for airport in AIRPORTS_DATA if airport['city_code'] == city_code]
+    results = [airport for airport in airports if airport["city_code"] == city_code.upper()]
+    if not results:
+        raise ValueError(f"No airport found with city code: {city_code}")
+    return results
 
 def get_airport_by_country_code(country_code):
-    """Retrieve airport data by country code."""
-    return [airport for airport in AIRPORTS_DATA if airport['country_code'] == country_code]
+    results = [airport for airport in airports if airport["country_code"] == country_code.upper()]
+    if not results:
+        raise ValueError(f"No airport found with country code: {country_code}")
+    return results
 
-def get_airport_by_continent(continent_code):
-    """Retrieve airport data by continent."""
-    return [airport for airport in AIRPORTS_DATA if airport['continent'] == continent_code]
+def get_airport_by_continent(continent):
+    results = [airport for airport in airports if airport["continent"] == continent.upper()]
+    if not results:
+        raise ValueError(f"No airport found in continent: {continent}")
+    return results
